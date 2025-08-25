ae104ac731283259b1a78251a8c21d7eb8b14f33
Nic <38214064+nicolai-wachenschwan@users.noreply.github.com>
2025-08-25 18:57:25 +0200

    feat: add frontend build

---
+++ streamlit_drawable_canvas/frontend/build/precache-manifest.328bf538799c2a8cffca14cfbcb1aab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a9866488b8e4f076d7d49599c252a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a42e9c925d51279fd58",
+    "url": "./static/css/main.15fab37c.chunk.css"
+  },
+  {
+    "revision": "2dc97c5e7b1595567aa9",
+    "url": "./static/js/2.8f880b18.chunk.js"
+  },
+  {
+    "revision": "349298fbb30f981c1395376b153ee9a6",
+    "url": "./static/js/2.8f880b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a42e9c925d51279fd58",
+    "url": "./static/js/main.9446bc5a.chunk.js"
+  },
+  {
+    "revision": "77e61c67eb4f95e15891",
+    "url": "./static/js/runtime-main.ff285346.js"
+  }
+]);
